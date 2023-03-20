38f5ee329c8fbc2a36932b0e1b6e803660d04461
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2023-03-20 11:10:44 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let popup = document.getElementById("popup");
+
+function openpage(){
+  popup.classList.add("open-page") 
+  
+  
+}
+
+function closepage(){
+    popup.classList.remove("open-page") 
+  }
+  
+
+
+
+
+
+
